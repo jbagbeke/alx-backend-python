a089f719d2598f457a60949401851f6048500bf9
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-15 14:01:54 +0000

    0x00. Python - Variable Annotations Mandatory Task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Concatenation function - type annotations
-"""
+"""()
 
 
 def concat(str1: str, str2: str) -> str:
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Returns floor of a number
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Takes the floor of a number
+    """
+
+    return math.floor(n)

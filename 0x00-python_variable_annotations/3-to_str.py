b997e6f34860ec88beec2c58eb3944474f4786fb
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-15 14:06:10 +0000

    0x00. Python - Variable Annotations Mandatory Task 2

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Returns string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float and returns string representation
+    """
+    
+    return str(n)

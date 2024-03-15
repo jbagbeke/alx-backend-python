f04d033cd3136c862f3d84e79f2354dfdae910b9
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-15 22:52:59 +0000

    0x00. Python - Variable Annotations Mandatory Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """
 Type annotated function
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple of the string and the sqaure of v as float
     """

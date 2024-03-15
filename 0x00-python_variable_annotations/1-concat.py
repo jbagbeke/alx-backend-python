eeaa7b88d7f62fab1405bb2b4f92e0f0dafffe6b
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-15 13:48:35 +0000

    0x00. Python - Variable Annotations Mandatory Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Concatenation function - type annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns a concatenated string
+    
+    Keyword arguments:
+    str1, str2 - String args
+    """
+    
+    return (str1 + str2)
+    

ddfc26c264850b128417dda5eefccf35bf2f6053
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-15 23:57:43 +0000

    0x00. Python - Variable Annotations Mandatory Task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Annoatates  an already written function
+"""
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]

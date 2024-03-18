419b3f2220a8fdfce0160f501e0217adc83bb932
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-18 21:53:55 +0000

    0x01. Python - Async Mandatory task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Async Basic Function declarations
+"""
+import asyncio
+from random import randint
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    rand_value = randint(0, max_delay)
+    await asyncio.sleep(rand_value)
+    return rand_value

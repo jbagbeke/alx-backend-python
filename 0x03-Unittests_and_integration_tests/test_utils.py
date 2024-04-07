ae06cba4119d5657837e68103173dbdd223037d3
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-07 16:26:41 +0000

    0x03. Unittests and Integration Tests Mandatory Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,7 +25,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError):f
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 

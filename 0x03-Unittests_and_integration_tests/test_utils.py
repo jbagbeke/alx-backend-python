405e12376505c8a6c62a41470ceeb4d68601ddea
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-07 18:23:52 +0000

    0x03. Unittests and Integration Tests Mandatory Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -55,6 +55,9 @@ class TestMemoize(unittest.TestCase):
     """
     def test_memoize(self):
         class TestClass:
+            """
+            Test class
+            """
             def a_method(self):
                 return 42
 

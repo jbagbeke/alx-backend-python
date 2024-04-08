7c0a23174252296da303778a341d8845fea51f32
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-08 22:26:37 +0000

    0x03. Unittests and Integration Tests Mandatory Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,9 +17,18 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('requests.get')
     def test_org(self, org_name, json_mock):
         json_mock.return_value.json.return_value = "https://holberton.io"
-        
+
         test_client = GithubOrgClient(org_name)
         test_client.org
         test_client.org
 
         json_mock.assert_called_once()
+
+    def test_public_repos_url(self):
+        test_client = GithubOrgClient("google")
+
+        with patch.object(GithubOrgClient, 'org', new=
+                {"repos_url": "https://api.github.com/repos"}):
+
+            self.assertEqual(test_client._public_repos_url,
+                             "https://api.github.com/repos")

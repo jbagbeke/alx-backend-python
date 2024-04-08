6daf4a6bea33a506f263c1bc2d013da43d795b60
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-08 21:24:13 +0000

    0x03. Unittests and Integration Tests Mandatory Task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+GITHUB CLIENT TESTING
+"""
+import unittest
+from utils import get_json, access_nested_map, memoize
+from client import GithubOrgClient
+from unittest.mock import patch
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Testing for github client
+    """
+    @parameterized.expand([("google"), ("abc")])
+    @patch('requests.get')
+    def test_org(self, org_name, json_mock):
+        json_mock.return_value.json.return_value = "https://holberton.io"
+        
+        test_client = GithubOrgClient(org_name)
+        test_client.org
+        test_client.org
+
+        json_mock.assert_called_once()
